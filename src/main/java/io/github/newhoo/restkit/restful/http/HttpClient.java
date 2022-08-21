fab1cbf59743aa1ddbdfb51986c2bcb7a49b911a
huzunrong <huzunrong@foxmail.com>
2022-08-21 17:07:49 +0800

    fix upload bug

--- src/main/java/io/github/newhoo/restkit/restful/http/HttpClient.java
+++ src/main/java/io/github/newhoo/restkit/restful/http/HttpClient.java
@@ -223,9 +223,9 @@ public class HttpClient implements RestClient {
                 sb.append("\n").append(request.getBody()).append("\n");
             } else if (request.getOriginal() instanceof HttpEntityEnclosingRequest) {
                 try {
-                    String s = "Content length is too long";
+                    String s = "[multipart form data ...]";
                     HttpEntity entity = ((HttpEntityEnclosingRequest) request.getOriginal()).getEntity();
-                    if (entity.getContentLength() <= 25 * 1024) { // org.apache.http.entity.mime.MultipartFormEntity.getContent
+                    if (entity instanceof UrlEncodedFormEntity) {
                         s = IOUtils.toString(entity.getContent(), StandardCharsets.UTF_8);
                     }
                     sb.append("\n").append(s).append("\n");
